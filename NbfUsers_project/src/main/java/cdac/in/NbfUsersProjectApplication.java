0b04a56b390ff645d1d30cdc872fa98d596b7de4
pratikshakhade <pratikshakhade98@gmail.com>
2023-05-25 14:32:39 +0530

    added comment in main file

--- NbfUsers_project/src/main/java/cdac/in/NbfUsersProjectApplication.java
+++ NbfUsers_project/src/main/java/cdac/in/NbfUsersProjectApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+// added spring validation
+//added role base 
+//added exception handling
 @SpringBootApplication
 @EnableWebSecurity
 @EnableJpaRepositories(basePackages = "cdac.in")
